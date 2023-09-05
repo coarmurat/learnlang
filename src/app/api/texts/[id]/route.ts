83c6e40b5ae5f9ffe3ea7ea188bfe4d22dc59ca8
Murat Coşar <coarmurat@gmail.com>
2023-09-05 22:35:02 +0300

    Middleware was added and sqlite CRUD API was made working. (3 lines were added to \next\dist\server\lib\server-ipc\invoke-request.js)

--- src/app/api/texts/[id]/route.ts
+++ src/app/api/texts/[id]/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import pool from '@/lib/db'
 import { isRowId } from '@/lib/RowIdChecker'
 
-export async function GET(request:Request, {params}:{params:{id:string}}) {
+export async function GET(request:Request,{ params }:{params:{id:string} }) {
 
     const rowId = isRowId(params.id)
 
@@ -37,3 +37,79 @@ export async function GET(request:Request, {params}:{params:{id:string}}) {
       
 }
 
+export async function DELETE(request:Request,{ params }:{params:{id:string} }) {
+    
+    const rowId = isRowId(params.id)
+
+    if(!rowId)  return  NextResponse.json({ status:false })
+
+    let db        
+    
+    try {
+    
+        db = await pool.acquire()
+    
+        if(!db) return  NextResponse.json({ status:false })
+
+        const result = await db.get(`delete from texts where id = ${rowId}`).then( (res:any) => res === undefined ? res = {} : res )
+
+        
+        console.log(result)
+
+        return NextResponse.json({ status:true, rowId })  
+
+    } catch (err) {
+        
+        console.log(err)
+
+        return  NextResponse.json({ status:false })
+
+    } finally{
+        
+        pool.release(db!)
+        
+    }
+
+}
+
+export async function PUT(request:Request,{ params }:{params:{id:string} }) {
+    
+    const rowId = isRowId(params.id)
+
+    if(!rowId)  return  NextResponse.json({ status:false })
+
+    const data = await request.json().catch( err => false )
+
+    if(!data || !data.text) return NextResponse.json({ status:false })
+
+    let db        
+    
+    try {
+    
+        db = await pool.acquire()
+    
+        if(!db) return  NextResponse.json({ status:false })
+
+        const stmt = await db.prepare('update texts set text = ? where id = ?')
+        
+        const result = await stmt.run(data.text, rowId)
+
+        console.log(result)
+
+        if(result.changes !== 1) return NextResponse.json({ status:false })
+
+        return NextResponse.json({ status:true, rowId })  
+
+    } catch (err) {
+        
+        console.log(err)
+
+        return  NextResponse.json({ status:false })
+
+    } finally{
+        
+        pool.release(db!)
+        
+    }
+
+}
--- src/app/api/texts/route.ts
+++ src/app/api/texts/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import pool from '@/lib/db'
 
 
-export async function GET( request:Request ) {
+export async function GET() {
 
     let db
 
@@ -32,3 +32,38 @@ export async function GET( request:Request ) {
       
 }
 
+export async function POST( request:Request ){
+    
+    const data = await request.json().catch( err => false )
+
+    if(!data || !data.text) return NextResponse.json({ status:false })
+
+    let db
+
+    try {
+        
+        db = await pool.acquire()
+
+        if(!db) return NextResponse.json({ status:false })
+
+        const stmt = await db.prepare('insert into texts (text) values(?)')
+        
+        const result = await stmt.run(data.text)
+
+        if(result.changes !== 1) return NextResponse.json({ status:false })
+
+    } catch (err) {
+        
+        console.log(err)
+
+        return  NextResponse.json({ status:false })
+
+    } finally{
+        
+        pool.release(db!)
+        
+    }
+
+    return NextResponse.json({ status:true })
+
+}
