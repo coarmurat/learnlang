72d51e9239efe18e81c5dfb7bbbb12aafdcab5e4
Murat Coşar <coarmurat@gmail.com>
2023-09-09 23:49:46 +0300

    Improvements have been made in Header Nav menu.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,30 @@
+"use client"
+
 import styles from './styles.module.css'
 import Link from 'next/link'
+import { useState } from 'react'
 
 export default function Header() {
     
+    const [isNavShowing, setIsNavShowing] = useState(false)
+
+    const toggleNavMenu = () => {
+        
+        setIsNavShowing((previous)=> !previous)
+
+    }
 
     return(
         <header className={styles.root}>
             <div className={styles.container}>
-                <div className={styles.hamburgerIcon}>
+                <button tabIndex={0} type={'button'} className={styles.hamburgerIcon} onClick={toggleNavMenu}>
                     <div></div>
                     <div></div>
                     <div></div>
-                </div>
+                </button>
                 <div className={styles.pageIndicator}>Layout</div>
                 <div className={styles.logo}></div>
-                <nav className={styles.nav}>
+                <nav className={`${styles.nav} ${isNavShowing?styles.showNav:''}`}>
                     <ul>
                         <li><Link href=''>Text List</Link></li>
                         <li><Link href=''>Training</Link></li>
