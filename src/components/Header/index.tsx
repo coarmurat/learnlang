d783346c45fe49cffa7be6f5759bcbe1a0f1cbee
Murat Coşar <coarmurat@gmail.com>
2023-09-12 01:02:15 +0300

    The useTitle hook was used to update the current page breadcrumb.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,16 @@
 "use client"
 
+
 import styles from './styles.module.css'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
+import useTitle from '@/hooks/useTitle'
 
 export default function Header() {
     
     const [isNavShowing, setIsNavShowing] = useState(false)
+    
+    const title = useTitle()
 
     const toggleNavMenu = () => {
         
@@ -38,12 +42,12 @@ export default function Header() {
                     <div></div>
                     <div></div>
                 </button>
-                <div className={styles.pageIndicator}>Layout</div>
+                <div className={styles.pageIndicator}>{title}</div>
                 <div className={styles.logo}></div>
                 <nav className={`${styles.nav} ${isNavShowing?styles.showNav:''}`}>
                     <ul>
-                        <li><Link href=''>Text List</Link></li>
-                        <li><Link href=''>Training</Link></li>
+                        <li><Link href='/'>Text List</Link></li>
+                        <li><Link href='/practice'>Practice</Link></li>
                         <li><Link href=''>Manage Texts</Link></li>
                         <li><Link href=''>How Works</Link></li>
                     </ul>
